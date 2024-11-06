c05e0f60970dfb196d2ca21e7ad0ced3ddbaebdb
Luiz Filipe Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-05 23:09:25 -0300

    feat/ circular progress bar

--- src/app/components/CircularProgressBar/index.tsx
+++ src/app/components/CircularProgressBar/index.tsx
@@ -1,8 +1,16 @@
+import { ProgressBarProps } from "@/app/types/progress"
 import styles from "./index.module.scss"
 
-const CircularProgressBar = () => {
+const CircularProgressBar = ({ name, percent }: ProgressBarProps) => {
     return (
-        <></>
+        <>
+            <div className={styles.progress_bar} style={{ background: `conic-gradient(#F5E100,#F5E100 ${percent}%, rgba(0,0,0,0) 0%)` }}>
+                <div className={styles.progress_bar_filler}>
+                    <span>{percent}%</span>
+                </div>
+            </div>
+            {name}
+        </>
     )
 }
 
---
+++ src/app/types/progress.ts
@@ -0,0 +1,4 @@
+export interface ProgressBarProps{
+    name: string
+    percent: number
+}
