6ef19066e0e76ef39227c211649e09111c6a16a7
Luiz Filipe Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-06 16:41:07 -0300

    feat/ spanish constants

--- src/app/constants/personalInfo.tsx
+++ src/app/constants/personalInfo.tsx
@@ -1,17 +1,21 @@
-const user = {
+import { UserType } from "../types/user"
+
+const user: UserType = {
     name: "Nome usuário",
     ocupation: {
-        PT: "Desenvolvedor Full Stack",
-        EN: "Full Stack Developer"
+        en: "Full Stack Developer",
+        es: "Desarrollador Full Stack",
+        pt: "Desenvolvedor Full Stack"
     },
     country: {
-        PT: "Brasil",
-        EN: "Brazil"
+        en: "Brazil",
+        es: "Brasil",
+        pt: "Brasil"
     },
     city: "Brasília",
     age: 30,
-    linkedin: "",
-    github: ""
+    linkedin: "https://www.linkedin.com/in/",
+    github: "https://github.com/"
 }
 
 export { user }
--- src/app/constants/skills.tsx
+++ src/app/constants/skills.tsx
@@ -1,46 +1,82 @@
 const skills = [
     {
-        name: "React",
+        name: {
+            en: "React",
+            es: "React",
+            pt: "React"
+        },
         percent: 100,
         section: "main"
     },
     {
-        name: "PHP",
+        name: {
+            en: "PHP",
+            es: "PHP",
+            pt: "PHP"
+        },
         percent: 100,
         section: "main"
     },
     {
-        name: "TypeScript",
+        name: {
+            en: "TypeScript",
+            es: "TypeScript",
+            pt: "TypeScript"
+        },
         percent: 100,
         section: "main"
     },
     {
-        name: "HTML",
+        name: {
+            en: "HTML",
+            es: "HTML",
+            pt: "HTML"
+        },
         percent: 95,
         section: "secondary"
     },
     {
-        name: "CSS",
+        name: {
+            en: "CSS",
+            es: "CSS",
+            pt: "CSS"
+        },
         percent: 95,
         section: "secondary"
     },
     {
-        name: "JavaScript",
+        name: {
+            en: "JavaScript",
+            es: "JavaScript",
+            pt: "JavaScript"
+        },
         percent: 95,
         section: "secondary"
     },
     {
-        name: "Git",
+        name: {
+            en: "Version control with Git",
+            es: "Control de versiones con Git",
+            pt: "Controle de versão com Git"
+        },
         percent: 100,
         section: "soft"
     },
     {
-        name: "Inglês",
+        name: {
+            en: "English",
+            es: "Inglés",
+            pt: "Inglês"
+        },
         percent: 100,
         section: "soft"
     },
     {
-        name: "Espanhol",
+        name: {
+            en: "Spanish",
+            es: "Español",
+            pt: "Espanhol"
+        },
         percent: 100,
         section: "soft"
     }
