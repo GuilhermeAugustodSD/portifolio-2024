6c6d9923198480d4b1dd5d1bb3b960ad20afe061
Luiz Filipe Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-05 23:09:51 -0300

    feat/ linear progress bar

--- src/app/components/LinearProgressBar/index.tsx
+++ src/app/components/LinearProgressBar/index.tsx
@@ -1,8 +1,17 @@
+import { ProgressBarProps } from "@/app/types/progress"
 import styles from "./index.module.scss"
 
-const LinearProgressBar = () => {
+const LinearProgressBar = ({ name, percent }: ProgressBarProps) => {
     return (
-        <></>
+        <>
+            <div className={styles.progress_bar_label}>
+                <span>{name}</span>
+                <span>{percent}%</span>
+            </div>
+            <div className={styles.progress_bar}>
+                <div className={styles.progress_bar_filler} style={{width: `${percent}%`}}></div>
+            </div>
+        </>
     )
 }
 
