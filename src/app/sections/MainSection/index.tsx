bb4c41f7f5315c748864c67f0ecaf5ad159cd9fe
Luiz Filipe Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-06 16:43:16 -0300

    feat/ main section

---
+++ src/app/sections/MainSection/index.tsx
@@ -0,0 +1,12 @@
+import styles from "./index.module.scss"
+
+import { SectionPropsType } from "@/app/types/sections"
+
+const MainSection = ({ language }: SectionPropsType) => {
+    return (
+        <section>
+        </section>
+    )
+}
+
+export { MainSection }
