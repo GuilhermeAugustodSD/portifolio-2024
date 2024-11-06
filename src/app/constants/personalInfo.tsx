edc09116d6a9520fee4125d9c136a0dbe0e73d42
Luiz Filipe Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-05 23:13:22 -0300

    feat/ first constants

---
+++ src/app/constants/personalInfo.tsx
@@ -0,0 +1,17 @@
+const user = {
+    name: "Nome usuário",
+    ocupation: {
+        PT: "Desenvolvedor Full Stack",
+        EN: "Full Stack Developer"
+    },
+    country: {
+        PT: "Brasil",
+        EN: "Brazil"
+    },
+    city: "Brasília",
+    age: 30,
+    linkedin: "",
+    github: ""
+}
+
+export { user }
---
+++ src/app/constants/skills.tsx
@@ -0,0 +1,49 @@
+const skills = [
+    {
+        name: "React",
+        percent: 100,
+        section: "main"
+    },
+    {
+        name: "PHP",
+        percent: 100,
+        section: "main"
+    },
+    {
+        name: "TypeScript",
+        percent: 100,
+        section: "main"
+    },
+    {
+        name: "HTML",
+        percent: 95,
+        section: "secondary"
+    },
+    {
+        name: "CSS",
+        percent: 95,
+        section: "secondary"
+    },
+    {
+        name: "JavaScript",
+        percent: 95,
+        section: "secondary"
+    },
+    {
+        name: "Git",
+        percent: 100,
+        section: "soft"
+    },
+    {
+        name: "Inglês",
+        percent: 100,
+        section: "soft"
+    },
+    {
+        name: "Espanhol",
+        percent: 100,
+        section: "soft"
+    }
+]
+
+export { skills }
