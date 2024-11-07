d2c1749baa83b2b467b418750565168c6ee40079
Guilherme Dantas <guilhermednts2@gmail.com>
2024-11-07 18:36:08 -0300

    feat/ novo componente: card de experiências

--- src/app/components/ExperienceCard/index.tsx
+++ src/app/components/ExperienceCard/index.tsx
@@ -1,8 +1,30 @@
+import { CardExperiencesType } from "@/app/types/cards"
 import styles from "./index.module.scss"
+import { months } from "@/app/constants/months"
+import { DictionaryLanguagesType } from "@/app/types/language"
 
-const ExperienceCard = () => {
+const ExperienceCard = ({ocupation, company, startDate, endDate, description, language}: CardExperiencesType) => {
+
+    const getDateFormated = () => {
+        const splitValues = startDate.split('-')
+        const mes = Number(splitValues[1])
+        const ano = splitValues[0]
+        const date = new Date(startDate)
+        
+        console.log(date);
+        
+        if (endDate == "*") return `${months[3].name[language as typeof DictionaryLanguagesType]}/${ano}-ATUAL`
+    }
+
     return (
         <div className={styles.cardExperiences}>
+            <div className={styles.infosPrincipais}>
+                <small>{getDateFormated()}</small>
+                <h6>{ocupation}</h6>
+                <span>{company}</span>
+            </div>
+            <div className={styles.line}></div>
+            <p>{description}</p>
         </div>
     )
 }
