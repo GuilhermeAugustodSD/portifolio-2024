0615ba7fc13ae79fe244ffed174418ff590892a6
Guilherme Dantas <guilhermednts2@gmail.com>
2024-11-07 18:36:37 -0300

    feat/ constante de meses

---
+++ src/app/constants/months.tsx
@@ -0,0 +1,17 @@
+const months = [
+  { name: { en: "JAN", es: "ENE", pt: "JAN" } },
+  { name: { en: "FEB", es: "FEB", pt: "FEV" } },
+  { name: { en: "MAR", es: "MAR", pt: "MAR" } },
+  { name: { en: "APR", es: "ABR", pt: "ABR" } },
+  { name: { en: "MAY", es: "MAY", pt: "MAI" } },
+  { name: { en: "JUN", es: "JUN", pt: "JUN" } },
+  { name: { en: "JUL", es: "JUL", pt: "JUL" } },
+  { name: { en: "AUG", es: "AGO", pt: "AGO" } },
+  { name: { en: "SEP", es: "SEP", pt: "SET" } },
+  { name: { en: "OCT", es: "OCT", pt: "OUT" } },
+  { name: { en: "NOV", es: "NOV", pt: "NOV" } },
+  { name: { en: "DEC", es: "DIC", pt: "DEZ" } }
+];
+
+
+export { months };
