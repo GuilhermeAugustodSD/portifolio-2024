4946c8b2a283f110040c98981938f6fbfdbb69a9
Luiz Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-07 09:41:15 -0300

    fix/ imagem sizes

--- src/app/sections/MainSection/HeaderSection/index.tsx
+++ src/app/sections/MainSection/HeaderSection/index.tsx
@@ -1,14 +1,11 @@
-import { user } from "@/app/constants/personalInfo"
 import styles from "./index.module.scss"
-import { SectionPropsType } from "@/app/types/sections"
-import { DictionaryType } from "@/app/types/language"
 import Image from "next/image"
 
 const HeaderSection = () => {
 
     return (
         <div>
-            <Image src="/background.jpeg" alt="" width={500} height={300} />
+            <Image src="/background.jpeg" alt="" width={1168} height={342} />
         </div>
     )
 }
