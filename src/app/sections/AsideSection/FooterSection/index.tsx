14e4d0856864e3ffc7e7d9782bace23458bb41bb
Guilherme Dantas <guilhermednts2@gmail.com>
2024-11-07 12:53:30 -0300

    feat/ inserção de ícones e estrutura HTML

--- src/app/sections/AsideSection/FooterSection/index.tsx
+++ src/app/sections/AsideSection/FooterSection/index.tsx
@@ -3,13 +3,25 @@ import styles from "./index.module.scss"
 import { SectionPropsType } from "@/app/types/sections"
 import { dictionary } from "@/app/constants/dictionary"
 import { DictionaryLanguagesType } from "@/app/types/language"
+import { FaLinkedin, FaGithubSquare  } from "react-icons/fa";
+import { IoMdDownload } from "react-icons/io";
 
 const FooterSection = ({ language }: SectionPropsType) => {
     return (
         <div className={styles.footerContainer}>
-            Linkedin: {user.linkedin}
-            GitHub: {user.github}
-            {dictionary.download[language as keyof DictionaryLanguagesType]} CV
+            <div className={styles.iconsContainer}>
+                <a href={user.linkedin} title="LinkedIn" target="_blank">
+                    <FaLinkedin size={30}/>
+                </a>
+
+                <a href={user.github} title="GitHub" rel="" target="_blank">
+                    <FaGithubSquare size={30}/>
+                </a>
+            </div>
+            <div className={styles.downloadContaier}>
+                {dictionary.download[language as keyof DictionaryLanguagesType]} CV
+                <IoMdDownload size={20}/>
+            </div>
         </div>
     )
 }
--- src/app/sections/AsideSection/SoftSkillsSection/index.tsx
+++ src/app/sections/AsideSection/SoftSkillsSection/index.tsx
@@ -2,18 +2,20 @@ import styles from "./index.module.scss"
 import { skills } from "@/app/constants/skills"
 import { DictionaryLanguagesType } from "@/app/types/language"
 import { SectionPropsType } from "@/app/types/sections"
+import { FaCheck } from "react-icons/fa";
 
 const SoftSkillsSection = ({ language }: SectionPropsType) => {
     return (
-        <>
+        <div className={styles.softSkillsContainer}>
             {skills.filter((skill) => skill.section == "soft").map((skill) => {
                 return (
-                    <div key={`softSkill-${skill.name[language as keyof DictionaryLanguagesType]}`}>
+                    <div className={styles.softSkill} key={`softSkill-${skill.name[language as keyof DictionaryLanguagesType]}`}>
+                        <FaCheck size={14} color="#F5E100"/>
                         {skill.name[language as keyof DictionaryLanguagesType]}
                     </div>
                 )
             })}
-        </>
+        </div>
     )
 }
 
