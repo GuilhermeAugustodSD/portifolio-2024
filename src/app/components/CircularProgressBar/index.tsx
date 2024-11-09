05de1e380cc1423a05c4535fa5cb4a2d43425783
Luiz Filipe Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-08 21:40:30 -0300

    feat/ animating circular progress bar

--- src/app/components/CircularProgressBar/index.tsx
+++ src/app/components/CircularProgressBar/index.tsx
@@ -1,10 +1,19 @@
+"use client"
 import { ProgressBarProps } from "@/app/types/progress"
 import styles from "./index.module.scss"
+import { CSSProperties, useEffect, useState } from "react"
 
 const CircularProgressBar = ({ name, percent }: ProgressBarProps) => {
+
+    const [circularPercent, setCircularPercent] = useState(0)
+ 
+    useEffect(()=>{
+        setCircularPercent(percent)
+    },[])
+
     return (
         <div className={styles.circularProgressContainer}>
-            <div className={styles.progress_bar} style={{ background: `conic-gradient(#F5E100,#F5E100 ${percent}%, rgba(0,0,0,0) 0%)` }}>
+            <div className={styles.progress_bar} style={{"--progress": `${circularPercent}%`} as CSSProperties}>
                 <div className={styles.progress_bar_filler}>
                     <span>{percent}%</span>
                 </div>
