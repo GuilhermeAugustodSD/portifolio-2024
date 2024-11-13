a43eefa9b11e77a9bb27415edf1382a5e6db678c
Luiz Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-13 01:52:12 -0300

    feat/ new constants

---
+++ src/app/constants/languages.tsx
@@ -0,0 +1,15 @@
+const languages = [
+    {
+        lang: "en"
+    },
+    {
+        lang: "es"
+    },
+    {
+        lang: "pt"
+    }
+]
+
+const defaultLanguage = "en"
+
+export { languages, defaultLanguage }
