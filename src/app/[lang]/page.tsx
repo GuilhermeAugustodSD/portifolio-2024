f5755e31f1be6810a34902a8d969bebeef0d0aeb
Luiz Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-13 01:54:26 -0300

    feat/ preparing metatags

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,12 +3,52 @@ import styles from "./page.module.scss";
 import { MainSection } from "../sections/MainSection";
 import { ControlsSection } from "../sections/ControlsSection";
 import { DarkModeHolder } from "../components/DarkModeHolder";
+import { languages } from "../constants/languages";
+import { Metadata } from "next";
+import { user } from "../constants/personalInfo";
+import { DictionaryLanguagesType } from "../types/language";
 
 const dynamicParams = false
 
-const MainPage = async ({ params }: {
-    params: Promise<{ lang: string }>
-}) => {
+const generateMetadata = async ({ params }: { params: Promise<{ lang: string }> }): Promise<Metadata> => {
+
+    const language = (await params).lang
+
+    const title = `${user.name} - ${user.ocupation[language as keyof DictionaryLanguagesType]}`
+
+    return {
+        robots: {
+            index: true,
+            follow: true
+        },
+        title: title,
+        description: "",
+        generator: 'Next.js',
+        keywords: ['Next.js', 'React', 'JavaScript'],
+        openGraph: {
+            title: title,
+            description: "",
+            url: "",
+            type: "website",
+            locale: language
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: title,
+            description: "",
+        },
+        alternates: {
+            canonical: 'https://nextjs.org',
+            languages: {
+                'en': 'https://nextjs.org/en',
+                'es': 'https://nextjs.org/de',
+            }
+        },
+        category: ""
+    }
+}
+
+const MainPage = async ({ params }: { params: Promise<{ lang: string }> }) => {
 
     const language = (await params).lang
 
@@ -23,8 +63,8 @@ const MainPage = async ({ params }: {
 }
 
 const generateStaticParams = async () => {
-    return [{ lang: 'en' }, { lang: 'pt' }, { lang: 'es' }]
+    return languages
 }
 
-export { generateStaticParams, dynamicParams }
+export { generateStaticParams, generateMetadata, dynamicParams }
 export default MainPage
