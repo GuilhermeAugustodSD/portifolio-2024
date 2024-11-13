d0165179c1476836b5992410708286b4d2133d66
Luiz Flores de Lima <luizf.floresdelima@gmail.com>
2024-11-12 23:41:28 -0300

    feat/ redirect to /en as default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
+import { useRouter } from "next/router";
 import styles from "./page.module.scss";
 import { AsideSection } from "./sections/AsideSection";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      {/* <AsideSection /> */}
-    </div>
-  );
+
+  redirect("/en")
+
+  return null
 }
