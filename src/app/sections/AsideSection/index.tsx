f855bf498c0c14509e4af0818cbbdea130720647
Guilherme Dantas <guilhermednts2@gmail.com>
2025-04-24 11:55:42 -0300

    feat/ ajuste no aside para implementação da lógica mobile

--- src/app/sections/AsideSection/index.tsx
+++ src/app/sections/AsideSection/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import styles from "./index.module.scss"
 
 import { BasicInfoSection } from "./BasicInfoSection"
@@ -7,19 +8,28 @@ import { SecondarySkillsSection } from "./SecondarySkillsSection"
 import { SoftSkillsSection } from "./SoftSkillsSection"
 import { SectionPropsType } from "@/app/types/sections"
 import { FooterSection } from "./FooterSection"
+import Image from "next/image"
+import { useState } from "react"
 
 const AsideSection = ({ language }: SectionPropsType) => {
+    const [isOpen, setIsOpen] = useState(false)
     return (
-        <aside className={styles.asideContainer}>
-            <IntroSection language={language} />
-            <div className={styles.skillsContainer}>
-                <BasicInfoSection language={language} />
-                <MainSkillsSection language={language} />
-                <SecondarySkillsSection language={language} />
-                <SoftSkillsSection language={language} />
+        <>
+            <aside className={`${styles.asideContainer} ${isOpen ? styles.asideOpen : ""}`}>
+                <IntroSection language={language} />
+                <div className={styles.skillsContainer}>
+                    <BasicInfoSection language={language} />
+                    <MainSkillsSection language={language} />
+                    <SecondarySkillsSection language={language} />
+                    <SoftSkillsSection language={language} />
+                </div>
+                <FooterSection language={language} />
+            </aside>
+            <div className={styles.avatarAbsolute} onClick={() => setIsOpen((prev) => !prev)}>
+                <Image  src="/user_icon.png" alt="" width={40} height={40} />
             </div>
-            <FooterSection language={language} />
-        </aside>
+        </>
+        
     )
 }
 
