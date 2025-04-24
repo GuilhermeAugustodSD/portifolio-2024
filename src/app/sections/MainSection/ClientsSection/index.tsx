3b4e25e181af3f336ee7ab6b1a001b78ed5f33e4
Guilherme Dantas <guilhermednts2@gmail.com>
2025-04-24 11:56:44 -0300

    feat/ colocando breakpoints no carrossel

--- src/app/sections/MainSection/ClientsSection/index.tsx
+++ src/app/sections/MainSection/ClientsSection/index.tsx
@@ -26,28 +26,44 @@ const ClientsSection = ({ language }: SectionPropsType) => {
                     slidesPerView={3}
                     keyboard={{
                         enabled: true,
-                      }}
-                      pagination={{
+                    }}
+                    pagination={{
                         clickable: true,
-                      }}
-                      navigation={true}
-                      modules={[Keyboard, Pagination, Navigation]}
+                    }}
+                    navigation={true}
+                    modules={[Keyboard, Pagination, Navigation]}
                     className={styles.cardsContainer}
+                    breakpoints={{
+                        320: {
+                            slidesPerView: 1,
+                            spaceBetween: 10,
+                        },
+                        768: {
+                            slidesPerView: 1,
+                            spaceBetween: 20,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                            spaceBetween: 20,
+                        },
+                        1440: {
+                            slidesPerView: 3,
+                            spaceBetween: 20,
+                        },
+                    }}
                 >
-                    {clients.map((client, idx) => {
-                        return (
-                            <SwiperSlide key={`client-${idx}`}>
-                                <CardRecommendation
-                                    language={language}
-                                    name={client.name}
-                                    description={client.description[language as keyof DictionaryLanguagesType]}
-                                    grade={client.grade}
-                                    picture={client.picture}
-                                    company={client.company}
-                                />
-                            </SwiperSlide>
-                        )
-                    })}
+                    {clients.map((client, idx) => (
+                        <SwiperSlide key={`client-${idx}`}>
+                            <CardRecommendation
+                                language={language}
+                                name={client.name}
+                                description={client.description[language as keyof DictionaryLanguagesType]}
+                                grade={client.grade}
+                                picture={client.picture}
+                                company={client.company}
+                            />
+                        </SwiperSlide>
+                    ))}
                 </Swiper>
             </div>
         </section>
